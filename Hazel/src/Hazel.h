7a7253fdd46956687a78b3a4d4f8e6a0ba9fa67f
labibahmed <12345678labib@gmail.com>
2022-01-05 19:47:54 -0500

    Setup basic application and entry point

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,8 @@
+#pragma once
+// For use by Hazel Application
+
+#include "Hazel/Application.h"
+
+//----------------Entry Point------------------
+#include "Hazel/EntryPoint.h"
+//---------------------------------------------
