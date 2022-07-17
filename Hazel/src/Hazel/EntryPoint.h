dc44a1067468a0b3170177aed96391c5253d4871
labibahmed <ashhab.ahmed@ryerson.ca>
2022-07-16 22:59:17 -0400

    updates

--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -4,6 +4,11 @@
 extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int argc, char** argv) {
+	Hazel::Log::Init();
+	HZ_CORE_WARN("Initialized Log!");
+	HZ_WARN("Initialized Log!");
+	HZ_INFO("Initialized Log!");
+	
 	auto app = Hazel::CreateApplication();
 	app->Run();
 	delete app;
